18bad0a206518b2b469c9a769470f72d477a7ef1
J3ldo <97303347+J3ldo@users.noreply.github.com>
2023-04-02 23:42:19 +0200

    Tidy up code and make small speed ups
    Moved "global check" to the top of the function
    Added return statements so the program doesnt crash
    Add __name__ == "__main__" because flex
    Other things im to lazy for to name

--- Limited Blacklister.py
+++ Limited Blacklister.py
@@ -9,14 +9,17 @@ ctypes.windll.kernel32.SetConsoleTitleW("Limited Blacklister")
 check = True
 
 def program():
+    global check  # Moved to top of page to make it look cleaner
+    
     keyworderror = f"{Back.RED}{Fore.BLACK}[ERROR]{Back.BLACK}{Fore.WHITE} Keyword not found"
     keyword = ""
-    keyword = str(input(f"{Back.BLUE}{Fore.BLACK}[INPUT]{Fore.WHITE}{Back.BLACK} Enter keyword: "))
+    keyword = input(f"{Back.BLUE}{Fore.BLACK}[INPUT]{Fore.WHITE}{Back.BLACK} Enter keyword: ")
     if keyword == "":
         print(f"{Back.RED}{Fore.BLACK}[ERROR]{Back.BLACK} Keyword not found")
         program()
+        return  # If it doesnt return it will continue after getting no keyword.
+    
     print(f"{Back.YELLOW}{Fore.BLACK}[PROCESSING]{Back.BLACK}{Fore.WHITE} Grabbing all IDS linked to keyword ")
-    global check
     response = requests.get(
         f"https://catalog.roblox.com/v1/search/items?category=Collectibles&keyword={keyword}&limit=120&subcategory=Collectibles"
     )
@@ -30,20 +33,22 @@ def program():
     # e.g.
     # [321570597, 56935215, 57603998, 16641209, 108829742, 57038857]
     try:   
-        just_the_ids = [datum["id"] for datum in ids_and_item_types]
+        just_the_ids = [str(datum["id"]) for datum in ids_and_item_types]  # Automatically convert to string.
     except:
         print(keyworderror)
         program()
+        return # If it doesn't return it will continue without any ids.
+        
     # e.g.
     # ['321570597', '56935215', '57603998', '16641209', '108829742', '57038857']
-    ids_as_strings_instead_of_numbers = [str(id_) for id_ in just_the_ids]
-    final = ", ".join(ids_as_strings_instead_of_numbers)
-    if not final and check == True :
+    final = ", ".join(just_the_ids)
+    if not final and check:
         check = False
         print(keyworderror)
         check = True
         program()
-    elif check == True:
+        
+    elif check:
         check = False
         with open(f'Results/{keyword}.txt', 'w') as f:
             f.write(final)
@@ -52,5 +57,7 @@ def program():
         print(f"{Back.BLACK}{Fore.WHITE}-----------------------")
         check = True
         program()
-program()
-
+        
+        
+if __name__ == "__main__":
+    program()
